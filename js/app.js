ffda1720e6677202b5e8efe62170364b98c9eb94
Mohamadajan <mohamadajan321@gmail.com>
2021-06-07 23:32:03 +0300

    added html js css file and do changes

---
+++ js/app.js
@@ -0,0 +1,62 @@
+" use strict";
+ let userName = prompt("Hello , Whats your name ?");
+ alert("Welcome to our website " + userName);
+
+ let favٍSeas= prompt("Do you think i like summer ?").toLowerCase();
+ if(favٍSeas === 'yes' || favٍSeas === 'y'){
+     alert('you are correct , Summer my favourite Season ^^');
+   //  console.log('correct');
+ }else if(favٍSeas === 'no' || favٍSeas === 'n'){
+     alert('you are wrong Summer my favourite season ');
+ }else{
+     alert('please type (yes) or (no) or (y) or (n)');
+ }
+
+
+ let myAge = prompt("Am i 23 years old ?").toLowerCase();
+ if(myAge === 'yes' || myAge === 'y'){
+     alert('you are correct , I am almost 23 ^^');
+     //console.log('correct');
+ }else if(myAge === 'no' || myAge === 'n'){
+     alert('you are wrong ');
+ }else{
+     alert('please type (yes) or (no) or (y) or (n)');
+ }
+
+
+  let favAni = prompt("what about animale? do you think i love cats ?").toLowerCase();
+ if(favAni === 'yes' || favAni === 'y'){
+     alert('you are correct , already i have one');
+     //console.log('correct');
+ }else if(favAni === 'no' || favAni === 'n'){
+     alert('you are wrong i love cats very match');
+ }else{
+     alert('please type (yes) or (no) or (y) or (n)');
+ }
+
+
+
+ let study = prompt("do you think i like studying ?").toLowerCase();
+ if(study === 'yes' || study === 'y'){
+     alert('you are wrong , i dont like studying ');
+     
+ }else if(study === 'no' || study === 'n'){
+     alert('you are correct  i dont like studing :)');
+    // console.log('correct');
+ }else{
+     alert('please type (yes) or (no) or (y) or (n)');
+ }
+
+
+
+ let favCl = prompt("do you think i like RealMadrid ?").toLowerCase();
+ if(favCl === 'yes' || favCl === 'y'){
+     alert('you are correct , I wish visit madrid someday ^^');
+    // console.log('correct');
+ }else if(favCl === 'no' || favCl === 'n'){
+     alert('you are wrong ,  I real madredesta ! ');
+ }else{
+     alert('please type (yes) or (no) or (y) or (n)');
+ }
+
+ alert(' thank you for your time ' + userName);
